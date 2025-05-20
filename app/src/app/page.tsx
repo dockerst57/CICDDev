c43ed897e762935171c3578a85318f57d3bec19d
parias.th <paritas.th@softthai.co.th>
2025-05-20 16:52:58 +0700

    update

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/assets/images/next.svg"
+          src="/Billing-app/assets/images/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
